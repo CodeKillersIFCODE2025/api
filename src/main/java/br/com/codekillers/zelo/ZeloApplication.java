20e4c09dc832412054e8c409eab25fd1ef767182
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2025-09-12 21:08:00 -0300

    feat: initial project

---
+++ src/main/java/br/com/codekillers/zelo/ZeloApplication.java
@@ -0,0 +1,13 @@
+package br.com.codekillers.zelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codekillers/zelo/ZeloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codekillers.zelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
